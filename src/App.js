52a8280a26a0a43aab8b083ec6f1293602251a1e
Deepak-kaushik41 <158473942+Deepak-kaushik41@users.noreply.github.com>
2024-09-29 12:19:41 +0530

    Delete src directory

--- src/App.js
+++
@@ -1,7 +0,0 @@
-
-    // const cardReplace = document.getElementById("cardReplace");
-    // cardReplace.addEventListener(onclick,(e) => {
-    // e.preventDefault();
-    // console.log("replaced");
-    // });
-
